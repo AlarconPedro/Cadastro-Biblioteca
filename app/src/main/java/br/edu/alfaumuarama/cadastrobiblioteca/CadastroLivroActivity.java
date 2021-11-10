c7819425b4f72b2a7709df485ffa82ebdf269fc4
Pedro Henrique Alarcon <pedrohenriquealarcon@gmail.com>
2021-11-10 16:29:20 -0300

    Cadastro V1.0.0

---
+++ app/src/main/java/br/edu/alfaumuarama/cadastrobiblioteca/CadastroLivroActivity.java
@@ -0,0 +1,14 @@
+package br.edu.alfaumuarama.cadastrobiblioteca;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CadastroLivroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_livro);
+    }
+}
