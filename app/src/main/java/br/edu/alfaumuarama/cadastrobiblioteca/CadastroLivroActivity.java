982afd50a94056d2b7f92f1c570cddba390795cd
Pedro Henrique Alarcon <pedrohenriquealarcon@gmail.com>
2021-11-11 17:51:56 -0300

    Cadastro V1.1.0 Alfa

--- app/src/main/java/br/edu/alfaumuarama/cadastrobiblioteca/CadastroLivroActivity.java
+++ app/src/main/java/br/edu/alfaumuarama/cadastrobiblioteca/CadastroLivroActivity.java
@@ -3,12 +3,19 @@ package br.edu.alfaumuarama.cadastrobiblioteca;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class CadastroLivroActivity extends AppCompatActivity {
+
+    EditText txtTitulo, txtAutor, txtDescricao, txtEditora, txtAno, txtNumPaginas;
+    Button btnAdicionar, btnExcluir, btnCancelar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro_livro);
+
+
     }
 }
--- app/src/main/java/br/edu/alfaumuarama/cadastrobiblioteca/db/BancoDeDados.java
+++ app/src/main/java/br/edu/alfaumuarama/cadastrobiblioteca/db/BancoDeDados.java
@@ -1,4 +1,17 @@
 package br.edu.alfaumuarama.cadastrobiblioteca.db;
 
+import android.database.sqlite.SQLiteDatabase;
+
 public class BancoDeDados {
+    private SQLiteDatabase banco;
+
+    private static final BancoDeDados dataBase = new BancoDeDados();
+    public static BancoDeDados getInstance() {
+        return dataBase;
+    }
+
+    public SQLiteDatabase getMeuBanco() {
+        return banco;
+    }
+
 }
